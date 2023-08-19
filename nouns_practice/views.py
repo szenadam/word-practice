aa66788c7c2c349a04ed85c15b5f7ed25a4e5752
szenadam <szenadam1@gmail.com>
2023-08-19 18:18:08 +0200

    Add session handling for correct answers

--- nouns_practice/views.py
+++ nouns_practice/views.py
@@ -17,13 +17,27 @@ def index(request):
     return render(request, "nouns/index.html", context)
 
 def practice(request):
+    if not "correct_answer_num" in request.session:
+        request.session["correct_answer_num"] = 0
+
+    if request.method == "POST":
+        user_answer = request.POST.get("user_answer")
+        if remove_accent(request.session.get("answer")) == user_answer:
+            request.session["correct_answer_num"] += 1
+
     word = "машина"
     soup = get_soup(word)
     words = extract_words(soup)
     cases = extract_cases(words)
     grammatical_cases = [(x, y) for x in ["sing", "plur"] for y in ["nom", "gen", "dat", "acc", "ins", "pre"]]
     chosen_case = random.choice(grammatical_cases)
-    context = {'word': word, 'chosen_case': chosen_case }
+    request.session["answer"] = cases[chosen_case[0]][chosen_case[1]]
+    context = {
+        'word': word, 
+        'chosen_case': chosen_case,
+        'answer': cases[chosen_case[0]][chosen_case[1]],
+        'correct_answer_num': request.session["correct_answer_num"]
+    }
     return render(request, "nouns/practice.html", context)
 
 def get_soup(chosen_word):
@@ -81,3 +95,41 @@ def extract_cases(words):
     cases["sing"]["pre"] = words[10]
     cases["plur"]["pre"] = words[11]
     return cases
+
+def remove_accent(word):
+    accent_dict = {
+        "А": "А́",
+        "Е": "Е́",
+        "И": "И́",
+        "О": "О́",
+        "У": "У́",
+        "Ы": "Ы́",
+        "Э": "Э́",
+        "Ю": "Ю́",
+        "Я": "Я́",
+        "а": "á",
+        "а": "а́",
+        "е": "é",
+        "е": "е́",
+        "ё": "ё",
+        "и": "и́",
+        "и": "и́",
+        "о": "ó",
+        "о": "о́",
+        "у": "у́",
+        "у": "у́",
+        "ы": "ы́",
+        "ы": "ы́",
+        "э": "э́",
+        "э": "э́",
+        "ю": "ю́",
+        "ю": "ю́",
+        "я": "я́",
+        "я": "я́",
+    }
+
+    for key, value in accent_dict.items():
+        word = word.replace(value, key)
+
+    return word
+
